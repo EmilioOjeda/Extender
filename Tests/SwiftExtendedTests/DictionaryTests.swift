8dd1621efb9a55c9594872a296f38525c6bf432a
Emilio Ojeda <emilio.ojeda.mendoza+github@gmail.com>
2023-07-07 11:35:58 -0600

    feat: add the merge (`+=`) and merging (`+`) operators to dictionaries

---
+++ Tests/SwiftExtendedTests/DictionaryTests.swift
@@ -0,0 +1,38 @@
+import XCTest
+@testable import SwiftExtended
+
+final class DictionaryTests: XCTestCase {
+    func testMergeOperator() throws {
+        // given
+        var dictionaryOne: [String: Any] = [
+            "stringValue": name,
+            "integerValue": 0
+        ]
+        let dictionaryTwo: [String: Any] = [
+            "stringValue": name,
+            "integerValue": 2
+        ]
+        // when
+        dictionaryOne += dictionaryTwo
+        // then
+        XCTAssertEqual(name, try XCTUnwrap(dictionaryOne["stringValue"] as? String))
+        XCTAssertEqual(2, try XCTUnwrap(dictionaryOne["integerValue"] as? Int))
+    }
+
+    func testMergingOperator() {
+        // given
+        let dictionaryOne: [String: Any] = [
+            "stringValue": name,
+            "integerValue": 0
+        ]
+        let dictionaryTwo: [String: Any] = [
+            "stringValue": name,
+            "integerValue": 2
+        ]
+        // when
+        let dictionaryThree = dictionaryOne + dictionaryTwo
+        // then
+        XCTAssertEqual(name, try XCTUnwrap(dictionaryThree["stringValue"] as? String))
+        XCTAssertEqual(2, try XCTUnwrap(dictionaryThree["integerValue"] as? Int))
+    }
+}
