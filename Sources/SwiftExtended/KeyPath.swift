bd0af0b17a82fa3114f382e78583e91f0c4aba29
Emilio Ojeda <emilio.ojeda.mendoza+github@gmail.com>
2023-08-08 11:35:40 -0600

    feat: enable the comparison between key paths and values

--- Sources/SwiftExtended/KeyPath.swift
+++ Sources/SwiftExtended/KeyPath.swift
@@ -64,3 +64,59 @@ public func != <Element, Value: Equatable>(
         element[keyPath: keyPath] != value
     }
 }
+
+/// It allows for comparing whether the value in the key path is greater than the given value.
+/// - Parameters:
+///   - keyPath: A key path from a specific root type to a specific resulting value type.
+///   - value: The value to compare against the resulting value of the key path.
+/// - Returns: A function returning the resulting boolean of the comparison.
+public func > <Element, Value: Comparable>(
+    keyPath: KeyPath<Element, Value>,
+    value: Value
+) -> (Element) -> Bool {
+    { element in
+        element[keyPath: keyPath] > value
+    }
+}
+
+/// It allows for comparing whether the value in the key path is greater than or equal to the given value.
+/// - Parameters:
+///   - keyPath: A key path from a specific root type to a specific resulting value type.
+///   - value: The value to compare against the resulting value of the key path.
+/// - Returns: A function returning the resulting boolean of the comparison.
+public func >= <Element, Value: Comparable>(
+    keyPath: KeyPath<Element, Value>,
+    value: Value
+) -> (Element) -> Bool {
+    { element in
+        element[keyPath: keyPath] >= value
+    }
+}
+
+/// It allows for comparing whether the value in the key path is less than the given value.
+/// - Parameters:
+///   - keyPath: A key path from a specific root type to a specific resulting value type.
+///   - value: The value to compare against the resulting value of the key path.
+/// - Returns: A function returning the resulting boolean of the comparison.
+public func < <Element, Value: Comparable>(
+    keyPath: KeyPath<Element, Value>,
+    value: Value
+) -> (Element) -> Bool {
+    { element in
+        element[keyPath: keyPath] < value
+    }
+}
+
+/// It allows for comparing whether the value in the key path is less than or equal to the given value.
+/// - Parameters:
+///   - keyPath: A key path from a specific root type to a specific resulting value type.
+///   - value: The value to compare against the resulting value of the key path.
+/// - Returns: A function returning the resulting boolean of the comparison.
+public func <= <Element, Value: Comparable>(
+    keyPath: KeyPath<Element, Value>,
+    value: Value
+) -> (Element) -> Bool {
+    { element in
+        element[keyPath: keyPath] <= value
+    }
+}
