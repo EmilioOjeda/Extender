8aa16383607ad6e623ec26782ba9490537665ef0
Emilio Ojeda <emilio.ojeda.mendoza+github@gmail.com>
2023-08-09 09:46:50 -0600

    build: add the `swiftlint` dependency to the package

--- Package.swift
+++ Package.swift
@@ -3,6 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "Extender",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v12), // <-- The minimum version required by `SwiftLintPlugin`.
+        .tvOS(.v14),
+        .watchOS(.v7),
+        .driverKit(.v20)
+    ],
     products: [
         .library(
             name: "SwiftExtended",
@@ -11,18 +18,42 @@ let package = Package(
             name: "FoundationExtended",
             targets: ["FoundationExtended"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(
+            url: "https://github.com/realm/SwiftLint",
+            from: "0.52.4"),
+    ],
     targets: [
         .target(
-            name: "SwiftExtended"),
+            name: "SwiftExtended",
+            plugins: [
+                .plugin(
+                    name: "SwiftLintPlugin",
+                    package: "SwiftLint")
+            ]),
         .testTarget(
             name: "SwiftExtendedTests",
-            dependencies: ["SwiftExtended"]),
+            dependencies: ["SwiftExtended"],
+            plugins: [
+                .plugin(
+                    name: "SwiftLintPlugin",
+                    package: "SwiftLint")
+            ]),
         .target(
             name: "FoundationExtended",
-            dependencies: ["SwiftExtended"]),
+            dependencies: ["SwiftExtended"],
+            plugins: [
+                .plugin(
+                    name: "SwiftLintPlugin",
+                    package: "SwiftLint")
+            ]),
         .testTarget(
             name: "FoundationExtendedTests",
-            dependencies: ["FoundationExtended"]),
+            dependencies: ["FoundationExtended"],
+            plugins: [
+                .plugin(
+                    name: "SwiftLintPlugin",
+                    package: "SwiftLint")
+            ]),
     ]
 )
